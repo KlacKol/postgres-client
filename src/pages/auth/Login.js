fda1c94b165c92695b5a754174f06ac008ac4d9e
KlacKol <cheshtouch@gmail.com>
2020-09-03 16:23:09 +0300

    ae

--- src/pages/auth/Login.js
+++ src/pages/auth/Login.js
@@ -61,7 +61,7 @@ const SignIn = () => {
                     <LockOutlinedIcon/>
                 </Avatar>
                 <Typography component="h1" variant="h5">
-                    Sign Iq
+                    Sign Iqasd
                 </Typography>
                 <ValidatorForm
                     className={classes.form}
