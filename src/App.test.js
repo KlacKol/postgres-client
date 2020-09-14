f9ef75ce93fa98f5808096cec5550434bd878b1c
KlacKol <cheshtouch@gmail.com>
2020-09-14 17:58:47 +0300

    tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,15 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
-
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe('test client', () => {
+  describe('login', () => {
+    it('should return string email', function () {
+      const isEmailOrName = (val) => {
+        const mailFormat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+        return val.match(mailFormat) ? 'email' : 'name';
+      };
+      expect(isEmailOrName('qwe@qwe.qwe')).toEqual('email');
+      expect(isEmailOrName('qwe.qwe')).toEqual('name');
+      expect(isEmailOrName('qwe@qwe')).toEqual('name');
+      expect(isEmailOrName('qwe')).toEqual('name');
+      expect(isEmailOrName('ches@gmail.com')).toEqual('email')
+    });
+  })
+})
